043c69e07fdfaeb19f392382245c3829c4d8f16c
kjconway5 <153889901+kjconway5@users.noreply.github.com>
2026-01-19 17:45:25 -0800

    hpf and lpf done and ready for unit testing

--- rtl/hpf/hpf.sv
+++ rtl/hpf/hpf.sv
@@ -1,9 +1,47 @@
-module hpf (#()
-    input  logic        clk,
-    input  logic        rst_n,
-    input  logic [15:0] in_data,
-    output logic [15:0] out_data
+module hpf 
+    #(parameter WIDTH = 16) (
+      input logic clk,
+      input logic reset,
+      input logic valid,
+      input logic signed [WIDTH-1:0] line_in,
+      output logic signed [WIDTH-1:0] line_out
 );
+
+    // High-pass filter used to remove low frequencies from the input signal
+    // used to remove low rumbles and DC offsets from our input
+    // takes in the input audio from an adc and outputs the same signal with low frequencies removed
+
+    // Implement a simple first-order high-pass filter using the difference equation: 
+    // out = a * [prev_out + (in - prev_in)]
+    // a is approx 1 
+    // sum - (sum >>> 6) is approx sum * (63/64)
+    // a can be changed to make the filter more or less aggressive
+
+    // MIGHT NEED TO CHANGE BIT WIDTHS TO AVOID OVERFLOW
+
+    logic signed [WIDTH-1:0] prev_line_out;
+    logic signed [WIDTH-1:0] prev_line_in;
+    logic signed [WIDTH-1:0] diff;
+    logic signed [WIDTH-1:0] sum;
+    logic signed [WIDTH-1:0] temp_out;
+
+    always_ff @(posedge clk) begin  
+        if (reset) begin 
+            line_out <= '0;
+            prev_line_out <= '0;
+            prev_line_in <= '0;
+        end else if (valid) begin
+            line_out <= temp_out;
+            prev_line_out <= temp_out;
+            prev_line_in <= line_in;
+        end
+    end
+
+    always_comb begin
+        diff = line_in - prev_line_in;
+        sum = prev_line_out + diff;
+        temp_out = sum - (sum >>> 6);
+    end
 
 
 endmodule
--- rtl/lpf/lpf.sv
+++ rtl/lpf/lpf.sv
@@ -1,9 +1,46 @@
-module lpf (#()
-    input  logic        clk,
-    input  logic        rst_n,
-    input  logic [15:0] in_data,
-    output logic [15:0] out_data
+module lpf 
+    #(parameter WIDTH = 16) (
+      input logic clk,
+      input logic reset,
+      input logic valid,
+      input logic signed [WIDTH-1:0] line_in,
+      output logic signed [WIDTH-1:0] line_out
 );
 
+    // Low-pass filter used to remove high frequencies from the input signal
+    // used to remove hissy sounds and high frequency noise from our input
+    // takes in the input audio from an adc and outputs the same signal with high frequencies removed
+
+    // Implement a simple first-order low-pass filter using the difference equation: 
+    // diff = line_in - prev_line_out
+	// scaled = a * diff
+	// y = prev_line_out + scaled
+	// store prev_line_out <= line_out
+    // a = 1/8
+
+    // line_out = prev_line_out + a * (line_in - prev_line_out)
+
+    // MIGHT NEED TO CHANGE BIT WIDTHS TO AVOID OVERFLOW
+
+    logic signed [WIDTH-1:0] prev_line_out;
+    logic signed [WIDTH-1:0] diff;
+    logic signed [WIDTH-1:0] scaled;
+    logic signed [WIDTH-1:0] temp_out;
+
+    always_ff @(posedge clk) begin  
+        if (reset) begin 
+            line_out <= '0;
+            prev_line_out <= '0;
+        end else if (valid) begin
+            line_out <= temp_out;
+            prev_line_out <= temp_out;
+        end
+    end
+
+    always_comb begin
+        diff = line_in - prev_line_out;
+        scaled = diff >>> 3;
+        temp_out = prev_line_out + scaled;
+    end
 
 endmodule
