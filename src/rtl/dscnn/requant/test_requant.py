85beb342b0973a4c0828275c414a9b7bb0040db8
kjconway5 <153889901+kjconway5@users.noreply.github.com>
2026-04-06 02:46:40 -0700

    negative tests and relu tests: requaant fully tested

--- src/rtl/dscnn/requant/test_requant.py
+++ src/rtl/dscnn/requant/test_requant.py
@@ -71,3 +71,80 @@ async def positive_saturation_test(dut):
     
     cocotb.log.info("positive_saturation_test passed")
     
+@cocotb.test()
+async def negative_test(dut):
+    
+    dut.relu_en.value = 0
+    dut.acc.value = -200
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 206, f"Expected out to be 206, got {dut.out.value}"
+    
+    dut.relu_en.value = 0
+    dut.acc.value = -100
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 231, f"Expected out to be 231, got {dut.out.value}"
+    
+    dut.relu_en.value = 1
+    dut.acc.value = -400
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0, f"Expected out to be 0, got {dut.out.value}"
+    
+    cocotb.log.info("negative_test passed")
+
+@cocotb.test()
+async def negative_saturation_test(dut):
+    
+    dut.relu_en.value = 0
+    dut.acc.value = -1025
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 128, f"Expected out to be 128, got {dut.out.value}"
+    
+    dut.relu_en.value = 1
+    dut.acc.value = -2048
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0, f"Expected out to be 0, got {dut.out.value}"
+    
+    cocotb.log.info("negative_saturation_test passed")
+    
+@cocotb.test()
+async def relu_test(dut):
+    dut.relu_en.value = 1
+    dut.acc.value = -200
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0, f"Expected out to be 0, got {dut.out.value}"
+    
+    dut.relu_en.value = 1
+    dut.acc.value = -100
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0, f"Expected out to be 0, got {dut.out.value}"
+    
+    dut.relu_en.value = 1
+    dut.acc.value = -400
+    dut.shift.value = 2
+    
+    await Timer(1, units='ns')
+
+    assert dut.out.value == 0, f"Expected out to be 0, got {dut.out.value}"
+    
+    cocotb.log.info("relu_test passed")
