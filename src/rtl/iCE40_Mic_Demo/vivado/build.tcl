0ca7e3b9aa291094093a4cd53d5ee736ec4630ef
Michael Aguero <mbaguero@ucsc.edu>
2026-04-16 19:41:39 -0700

    fixed file paths

--- src/rtl/iCE40_Mic_Demo/vivado/build.tcl
+++ src/rtl/iCE40_Mic_Demo/vivado/build.tcl
@@ -1,7 +1,7 @@
-read_verilog ../rtl/top.sv
-read_verilog ../rtl/fastfir.v
-read_verilog ../rtl/firtap.v
-read_verilog ../rtl/cic_decimator.v
+read_verilog ../top.sv
+read_verilog ../../FIR/fastfir.v
+read_verilog ../../FIR/firtap.v
+read_verilog ../../CIC/cic_decimator.v
 
 synth_design -top top -part xc7a35tcsg324-1
 
