2c88dc5e3c606ab555dca041768e76d5dd74ac1f
Aydan Olaez <aolaez@ucsc.edu>
2026-06-28 05:06:30 -0700

    fix: updated flake files for new template version and fixed timing on this new version

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   };
 
   inputs = {
-    librelane.url = "github:librelane/librelane/3.0.0";
+    librelane.url = "github:librelane/librelane/dev";
   };
 
   outputs =
@@ -69,16 +69,12 @@
               ps: with ps; [
                 # Verification
                 cocotb
-                numpy
-                scipy
 
                 # For KLayout Python DRC runner
                 docopt
 
                 # For logo generation
                 pillow
-
-          
               ];
           });
         }
